540df7c82596b391df0cbc6cea0d17bd39ec144f
Vivek Kamble <mrvivekkamble8@gmail.com>
2025-08-02 03:41:43 +0530

    Fix: errors and deployment errors

--- app/provider.jsx
+++ app/provider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function provider() {
   
-  const createNewUser = async (email, password) => {
+//   const createNewUser = async (email, password) => {
     return (
     <div>
 
